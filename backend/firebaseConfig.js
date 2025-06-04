c0faba25dade612c1784d2097b71124466fb630a
shirin17h <shirin17h@gmail.com>
2025-06-04 17:56:55 +0300

    fixed firebase config

--- backend/firebaseConfig.js
+++ backend/firebaseConfig.js
@@ -1,11 +1,11 @@
 // Firebase Admin SDK initialization and Firestore DB setup
-
+require("dotenv").config();
 const admin = require("firebase-admin");
 
-// const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
 
 //connect to firebase DB
-admin.initializeApp({ credential: admin.credential.cert("serviceAccountKey.json") });
+admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 const db = admin.firestore();
 
 module.exports = { admin, db };
