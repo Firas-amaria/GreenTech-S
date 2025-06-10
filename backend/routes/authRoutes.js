aae603a2994196fd40476cac5bad0c1223040427
Ayebk <ashmoz676@gmail.com>
2025-06-10 20:59:49 +0300

    Updated routes user,farmer,auth

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 const {
   registerCustomer,
-  registerEmployee,
+  requestEmployment,
   getUserRole,
   login,
 } = require("../controllers/authController");
 
 // Customer and employee registration routes
 router.post("/register-customer", registerCustomer); // for customers
-router.post("/register-employee", registerEmployee); // for job applicants
+router.post("/register-employee", requestEmployment); // for job applicants
 router.post("/get-role", getUserRole);
 router.post("/login", login); // login
 module.exports = router;
