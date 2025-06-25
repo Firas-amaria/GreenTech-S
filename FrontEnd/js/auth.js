7052716d3c9a50c11a52ff0e9c088ba9e9c99ea4
Firas-amaria <128461535+Firas-amaria@users.noreply.github.com>
2025-06-25 12:03:50 +0300

    Refactor: Simplify registration and login processes, enhance error handling, and clean up commented code

--- FrontEnd/js/auth.js
+++ FrontEnd/js/auth.js
@@ -1,10 +1,5 @@
 // js/auth.js
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 import { auth, getCurrentUserToken } from "./firebase-init.js";
 
@@ -16,7 +11,7 @@ window.register = async (event) => {
   const lastName = form["register-fullname"].value.split(" ")[1] || "";
 
   const email = form["register-email"].value;
-  const phone = form["register-phone"].value;
+  const phone = iti.getNumber(); // This gives the full international number like +972512325456
   const address = form["register-address"].value;
   const birthDate = form["register-birthdate"].value;
   const password = form["register-password"].value;
@@ -28,9 +23,8 @@ window.register = async (event) => {
     form["register-fullname"].style.borderColor = "red";
     return;
   }
-  console.log(firstName.length);
   if (firstName.length < 2) {
-    console.log("aaa");
+    // console.log("aaa");
     document.getElementById("error-message").innerText =
       "First name must be at least 2 characters long.";
     form["register-fullname"].style.borderColor = "red";
@@ -172,7 +166,6 @@ window.register = async (event) => {
   }
 
   try {
-    // Send user data to backend
     const res = await fetch(
       "http://localhost:4000/api/auth/register-customer",
       {
@@ -192,13 +185,20 @@ window.register = async (event) => {
         }),
       }
     );
+
     const data = await res.json();
-    console.log(data);
+
+    if (!res.ok) {
+      // Show backend error message if registration fails
+      alert("Registration failed: " + data.error);
+      return;
+    }
+
     alert("Registration successful!");
     window.location.href = "login.html";
   } catch (error) {
     console.error(error);
-    document.getElementById("error-message").innerText = error.message;
+    alert("Registration failed - Unknown Error ");
   }
 };
 
@@ -216,14 +216,13 @@ window.login = async (event) => {
     );
 
     const user = userCredential.user;
-    console.log(user);
+    // console.log(user.uid " " + user.email);
 
     // Get token using shared utility
     const token = await getCurrentUserToken();
-    console.log(user.uid);
 
     // Post password user to DB
-    await fetch("http://localhost:4000/api/auth/login", {
+    const res = await fetch("http://localhost:4000/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -232,23 +231,24 @@ window.login = async (event) => {
       body: JSON.stringify({ uid: user.uid, password }),
     });
 
-    // Fetch user info from backend
-    const res = await fetch("http://localhost:4000/api/user/profile", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    // // Fetch user info from backend
+    // await fetch("http://localhost:4000/api/user/profile", {
+    //   method: "GET",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Authorization: `Bearer ${token}`,
+    //   },
+    // });
 
     const data = await res.json();
     // console.log("User data from backend:", data);
 
-    const role = data.position || data.role; // role or position field
-    const name = data.firstName + " " + data.lastName || "";
+    const role = data.role;
+    const name = data.name;
 
-    console.log("User role:", role);
+    // console.log("User role:", role);
 
+    //save name and role in localStorage to use later
     localStorage.setItem(
       "user",
       JSON.stringify({
@@ -256,6 +256,8 @@ window.login = async (event) => {
         name: name,
       })
     );
+
+    // alert(`Welcome ${name}! You are logged in as ${role}.`);
 
     // Redirect based on role
     switch (role) {
