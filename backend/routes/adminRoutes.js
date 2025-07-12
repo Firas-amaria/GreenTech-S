69c1d4b90af94533cfe9f32c164c2f3176fdc982
shirin17h <shirin17h@gmail.com>
2025-07-12 20:12:48 +0300

    refactor: streamline route definitions for better readability in adminRoutes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -19,33 +19,20 @@ getOrdersWithSummaryForShift,
 // router.post("/import-json", importJsonToFirestore);
 
 /*DASHBOARD*/
-router.get(
-  "/orders-for-upcoming-shifts",
-  authenticate,
-  requireRole("admin"),
-  getOrdersForUpcomingShifts
-);
-router.get("/orders-for-shift", authenticate, requireRole("admin"), getOrdersForShift);
-
-router.get("/orders-with-summary-for-shift", authenticate, requireRole("admin"), getOrdersWithSummaryForShift);
+router.get("/orders-for-upcoming-shifts", authenticate, requireRole("admin"), 
+getOrdersForUpcomingShifts);
+router.get("/orders-for-shift", authenticate, requireRole("admin"), 
+getOrdersForShift);
+router.get("/orders-with-summary-for-shift", authenticate, requireRole("admin"), 
+getOrdersWithSummaryForShift);
 
 /*JOB APPLICATION AND USER MANAGEMENT*/ 
-router.put(
-  "/updateApplication/:uid",
-  authenticate,
-  requireRole("admin"),
-  updateApplicationStatus
-); // Route restricted to admin for approving employee accounts
+router.put("/updateApplication/:uid", authenticate, requireRole("admin"), updateApplicationStatus); // Route restricted to admin for approving employee accounts
 router.get("/application", authenticate, requireRole("admin"), getApplication); // Get user's employment application
 router.post("/set-role", authenticate, requireRole("admin"), setRole);
 router.get("/profile/:id", authenticate, requireRole("admin"), getProfileById); // get any user's profile
 router.get("/users", authenticate, requireRole("admin"), getAllUsers); // get all users
-router.get(
-  "/getApplications",
-  authenticate,
-  requireRole("admin"),
-  getAllApplications
-); // get all employment applications
+router.get("/getApplications", authenticate, requireRole("admin"),getAllApplications); // get all employment applications
 router.put("/users/:id", authenticate, requireRole("admin"), updateUser);
 router.delete("/users/:id", authenticate, requireRole("admin"), deleteUser);
 
